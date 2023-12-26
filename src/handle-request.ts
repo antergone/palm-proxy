e6e13f3c9e34d5f176c07b8126bc28e4f9e16f38
Simon Ma <chnmayu@gmail.com>
2023-12-26 12:24:27 +0000

    add index html

--- src/handle-request.ts
+++ src/handle-request.ts
@@ -28,6 +28,33 @@ export default async function handleRequest(request: NextRequest & { nextUrl?: U
 
   const { pathname, searchParams } = request.nextUrl ? request.nextUrl : new URL(request.url);
 
+  if(pathname === "/") {
+    let blank_html = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Google PaLM API proxy on Vercel Edge</title>
+</head>
+<body>
+  <h1 id="google-palm-api-proxy-on-vercel-edge">Google PaLM API proxy on Vercel Edge</h1>
+  <p>Tips: This project uses a reverse proxy to solve problems such as location restrictions in Google APIs. </p>
+  <p>If you have any of the following requirements, you may need the support of this project.</p>
+  <ol>
+  <li>When you see the error message &quot;User location is not supported for the API use&quot; when calling the Google PaLM API</li>
+  <li>You want to customize the Google PaLM API</li>
+  </ol>
+  <p>For technical discussions, please visit <a href="https://simonmy.com/posts/使用vercel反向代理google-palm-api.html">https://simonmy.com/posts/使用vercel反向代理google-palm-api.html</a></p>
+</body>
+</html>
+    `
+    return new Response(blank_html, {
+      headers: {
+        ...CORS_HEADERS,
+        "content-type": "text/html"
+      },
+    });
+  }
   // curl \
   // -H 'Content-Type: application/json' \
   // -d '{ "prompt": { "text": "Write a story about a magic backpack"} }' \
